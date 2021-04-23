0598fa7bf6ff281c367ab8e12f3062f44ccf398c
Pietro Lima <pietrovinicius@hotmail.com>
2021-04-23 20:46:45 -0300

    Alterar o layout raiz para usar LinearLayout

--- app/src/main/java/com/example/sobremim/MainActivity.kt
+++ app/src/main/java/com/example/sobremim/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.sobremim
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //escondendo a status bar
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
     }
 }
